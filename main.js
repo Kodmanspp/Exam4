9ce052a1ce3627a51fa94b220f7cda1ed0b0f5b8
kodmanspp <k.bayaman@bk.ru>
2021-08-20 03:16:34 +0600

    end Exam 4

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 
 const createTask = document.querySelector(".create-task_btn");
+const createTask_btn = document.querySelector('.modal__save-task_btn');
 
-const check = document.querySelectorAll(".task__checkbox");
+const taskCheck = document.getElementsByClassName('task__checkbox');
 const deleteTask = document.querySelectorAll(".task__delete_btn");
-const changeBtn = document.querySelectorAll(".text__change");
+
 
 const main = document.querySelector(".main");
+const title = document.querySelector('.modal__title');
 
 
-const createTask_btn = document.querySelector('.modal__save-task_btn')
 
 
+
+// Render function 
+const renderTasks = () => {
+    for (let i = 0; i < tasks.length; i++) {
+        main.appendChild(createTaskTag(tasks[i].title, tasks[i].text, tasks[i].checked));
+    }
+}
+function allFunc() {
+    renderTasks();
+    TaskChecked();
+    deleteTasks_Btn();
+    taskChange();
+}
+
+// Create function
 const createElement = (tag, className, textContent) => {
     if (!tag) {
         alert("Внутреняя ошибка сервиса!");
@@ -45,105 +61,19 @@ function createTaskTag(title, text, checked) {
     </div>
     <img class="task__delete_btn" src="./assets/delete__btn.png" alt="">
     </div>`;
-    if(checked === 1){
+    if (checked === 1) {
         const titleChecked = element.querySelector('.task__title');
+        const check_img = element.querySelector('.task__checkbox_img');
+        openModal(check_img);
         titleChecked.style.textDecoration = 'line-through';
     }
-    const delete_btn = element.querySelector('.task__delete_btn');
-    delete_btn.addEventListener('click', function(){
-            const deleteTaskModal = document.querySelector(".modal-delete");
-            const modalClose_Btn = document.querySelector(".modal-delete__close_btn")
-            const modalClose = document.querySelector(".modal__close__2");
-    
-            deleteTaskModal.style.visibility = 'visible';
-            deleteTaskModal.style.opacity = 1;
-    
-            modalClose.onclick = function () {
-                closeModal(deleteTaskModal);
-            };
-            modalClose_Btn.onclick = function () {
-                closeModal(deleteTaskModal);
-            };
-            
-            // window.onclick = function (event) {
-            //     if (event.target == deleteTaskModal) {
-            //         closeModal(deleteTaskModal);
-            //     }
-            // }
-      
-    })
     return element;
 }
-createTask_btn.addEventListener('click', function(){
-
-    const titleInput = document.querySelector('.modal__title-input');
-    const textInput = document.querySelector('.modal__text');
-    const createTaskModal = document.querySelector(".modal");
-
-    const element = {
-        title: '', 
-        text: '',
-        checked: 0
-    }
-    if (!titleInput.value || titleInput.value.trim().length === 0) {
-        let error = document.querySelector('.modal__error');
-        error.textContent = 'Вы не ввели заголовок';
-        openModal(error);  
-        return; 
-    }
-    else if (textInput.value === undefined) {
-        text = '';
-    }
-    element.title = titleInput.value;
-    element.text = textInput.value;
-    tasks.push(element);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    main.innerHTML ='';
-    renderTasks();
-    closeModal(createTaskModal);
-})
-const renderTasks = () => {
-    for (let i = 0; i < tasks.length; i++) {
-        main.appendChild(createTaskTag(tasks[i].title, tasks[i].text, tasks[i].checked)); 
-    }
-}
-renderTasks();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 createTask.addEventListener('click', function () {
     const createTaskModal = document.querySelector(".modal");
     const modalClose = document.querySelector('.modal__close');
     const closeModal_btn = document.querySelector('.modal__close_btn');
-
+    title.innerHTML = 'Создать задачу';
     createTaskModal.style.visibility = 'visible';
     createTaskModal.style.opacity = 1;
 
@@ -153,29 +83,67 @@ createTask.addEventListener('click', function () {
     closeModal_btn.onclick = function () {
         closeModal(createTaskModal);
     };
-})
-deleteTask.forEach(function (element) {
-    element.addEventListener('click', function () {
-        const deleteTaskModal = document.querySelector(".modal-delete");
-        const modalClose_Btn = document.querySelector(".modal-delete__close_btn")
-        const modalClose = document.querySelector(".modal__close__2");
+    clearInput();
+    createTask_btn.onclick = createTask_Save;
 
-        deleteTaskModal.style.visibility = 'visible';
-        deleteTaskModal.style.opacity = 1;
-
-        modalClose.onclick = function () {
-            closeModal(deleteTaskModal);
-        };
-        modalClose_Btn.onclick = function () {
-            closeModal(deleteTaskModal);
-        };
-        // window.onclick = function (event) {
-        //     if (event.target == deleteTaskModal) {
-        //         closeModal(deleteTaskModal);
-        //     }
-        // }
-    });
 })
+
+function createTask_Save() {
+    const titleInput = document.querySelector('.modal__title-input');
+    const textInput = document.querySelector('.modal__text');
+    const createTaskModal = document.querySelector(".modal");
+    
+    const element = {
+        title: '',
+        text: '',
+        checked: -1,
+    }
+    if (!titleInput.value || titleInput.value.trim().length === 0) {
+        let error = document.querySelector('.modal__error');
+        error.textContent = 'Вы не ввели заголовок';
+        openModal(error);
+        return;
+    }
+    else if (textInput.value === undefined) {
+        text = '';
+    }
+    element.title = titleInput.value;
+    element.text = textInput.value;
+    tasks.push(element);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    main.innerHTML = '';
+    allFunc();
+    closeModal(createTaskModal);
+    
+}
+function changeTask_Save(index){
+    const titleInput = document.querySelector('.modal__title-input');
+    const textInput = document.querySelector('.modal__text');
+    const createTaskModal = document.querySelector(".modal");
+    const element = {
+        title: '',
+        text: '',
+        checked: -1,
+    }
+    if (!titleInput.value || titleInput.value.trim().length === 0) {
+        let error = document.querySelector('.modal__error');
+        error.textContent = 'Вы не ввели заголовок';
+        openModal(error);
+        return;
+    }
+    else if (textInput.value === undefined) {
+        text = '';
+    }
+    element.title = titleInput.value;
+    element.text = textInput.value;
+    tasks[index] = element;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    main.innerHTML = '';
+    allFunc();
+    closeModal(createTaskModal);
+    
+}
+// Modal activate fucntion 
 const closeModal = function (item) {
     item.style.visibility = 'hidden';
     item.style.opacity = 0;
@@ -184,3 +152,75 @@ const openModal = function (item) {
     item.style.visibility = 'visible';
     item.style.opacity = 1;
 };
+
+function clearInput(){
+    document.querySelector('.modal__title-input').value = 'Заголовок задачи';
+    document.querySelector('.modal__text').value = '';
+}
+
+// add functional for task
+function deleteTasks_Btn() {
+    const deleteTask_btn = document.getElementsByClassName('task__delete_btn');
+    for (let i = 0; i < deleteTask_btn.length; i++) {
+        deleteTask_btn[i].onclick = function () {
+            const deleteTaskModal = document.querySelector(".modal-delete");
+            const modalClose_Btn = document.querySelector(".modal-delete__close_btn")
+            const modalClose = document.querySelector(".modal__close__2");
+            openModal(deleteTaskModal);
+            modalClose.onclick = function () {
+                closeModal(deleteTaskModal);
+            };
+            modalClose_Btn.onclick = function () {
+                closeModal(deleteTaskModal);
+            };
+            const deleteTask = document.querySelector('.modal-delete__btn');
+            deleteTask.onclick = () => {
+                tasks.splice(i, 1);
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+                main.innerHTML = '';
+                allFunc();
+                closeModal(deleteTaskModal);
+            }
+        }
+    }
+}
+function taskChange() {
+    const taskChange = document.getElementsByClassName('change');
+    for (let i = 0; i < taskChange.length; i++) {
+        taskChange[i].onclick = function () {
+            const createTaskModal = document.querySelector(".modal");
+            const modalClose = document.querySelector('.modal__close');
+            const closeModal_btn = document.querySelector('.modal__close_btn');
+            title.textContent = 'Изменить задачу';
+            createTaskModal.style.visibility = 'visible';
+            createTaskModal.style.opacity = 1;
+
+            modalClose.onclick = function () {
+                closeModal(createTaskModal);
+            };
+            closeModal_btn.onclick = function () {
+                closeModal(createTaskModal);
+            };
+            clearInput();
+            createTask_btn.onclick = function(){
+                changeTask_Save(i);
+                
+            };
+            
+        }
+    }
+}
+function TaskChecked() {
+    for (let i = 0; i < taskCheck.length; i++) {
+        taskCheck[i].onclick = function () {
+            tasks[i].checked *= -1;
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            main.innerHTML = '';
+            allFunc();
+        };
+    }
+}
+allFunc();
+
+
+
